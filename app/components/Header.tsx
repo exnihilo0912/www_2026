da3cb07c19bf952c78e80859800ca76bfec1085f
Adam EMMANUEL <adam.emmanuel.pro@gmail.com>
2026-01-24 17:16:45 +0100

    [feat] get path for header behavior

---
+++ app/components/Header.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { HomeIcon, UserCircleIcon, QuestionMarkCircleIcon, CommandLineIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const menuItems = [
+  { id: 'home', label: 'Home', icon: <HomeIcon />, path: '/' },
+  { id: 'resume', label: 'About', icon: <UserCircleIcon />, path: '/about' },
+  { id: 'projects', label: 'Projects', icon: <CommandLineIcon />, path: '/projects' },
+  { id: 'misc', label: 'Misc.', icon: <QuestionMarkCircleIcon />, path: '/misc' },
+];
+
+export default function Header() {
+  // TODO add url-base logic
+  const pathname = usePathname();
+  const segments = pathname.split('/');
+  // if segments.length > 1, not root => shrink header
+
+  return (
+    <header className="flex items-center justify-between h-[64] fixed w-96/100 md:w-45/100 rounded-[12] top-[8] md:top-[16] left-['calc(50% - 96% / 2)'] pl-[16] pr-[16] bg-base shadow-basic shadow-base-shadow">
+      <nav>
+        <ul className="flex gap-6 md:gap-3">
+          {menuItems.map(({ id, icon, path }) => 
+            <li key={id} className="flex items-center justify-center size-[32] text-stone-500 hover:text-stone-800 hover:bg-panel hover:rounded-full">
+              <Link href={path}>
+                <div className="size-[24]">{icon}</div>
+              </Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <div>{pathname}</div>
+    </header>
+  );
+}
+
+// Simplified breadcrumbs logic
+function Breadcrumbs() {
+  const pathname = usePathname()
+  const segments = pathname.split('/')
+
+  return (
+    <nav>
+      {segments.map((segment, index) => (
+        <span key={index}>
+          {' > '}
+          {segment}
+        </span>
+      ))}
+    </nav>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { HomeIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link'
 import "./globals.css";
+
+import Header from '@/app/components/Header';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,44 +19,7 @@ export const metadata: Metadata = {
   description: "Adam Emmanuel -- Portfolio - 2026",
 };
 
-interface MenuItem {
-  id: string;
-  label: string;
-  icon: string;
-  path: string;
-}
-
-const menuItems: MenuItem[] = [
-  { id: 'home', label: 'Home', icon: '〶', path: '/' },
-  { id: 'resume', label: 'Resume', icon: '✿', path: '/resume' },
-  { id: 'about', label: 'About me', icon: '⏀', path: '/about' },
-  { id: 'portfolio', label: 'Portfolio', icon: '⍄', path: '/portfolio' },
-];
-
-function Header() {
-
-  return (
-    <header className="flex items-center h-[64] fixed w-96/100 md:w-45/100 rounded-[12] top-[8] md:top-[16] left-['calc(50% - 96% / 2)'] pl-[16] pr-[16] bg-base shadow-basic shadow-base-shadow">
-      <nav>
-        <ul className="flex gap-3">
-          {menuItems.map(({ id, icon }) => <li key={id} className="flex items-center justify-center size-[32] bg-panel rounded-full"> <div className="size-[24]"><HomeIcon /></div></li> )}
-        </ul>
-      </nav>
-    </header>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="w-full h-48 p-3 bg-panel rounded-[12] border border-panel">
-      <div>blabla</div>
-    </footer>
-  );
-}
-
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function RootLayout({ children }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
