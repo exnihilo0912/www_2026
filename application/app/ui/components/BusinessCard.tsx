8372d4f11fdd9a0d01a901a17769c43c5448628e
Adam EMMANUEL <adam.emmanuel.pro@gmail.com>
2026-02-03 09:53:52 +0100

    [refactor] clean code -- BusinessCard

--- application/app/ui/components/BusinessCard.tsx
+++ application/app/ui/components/BusinessCard.tsx
@@ -5,26 +5,25 @@ const presentation = {
   title: 'Fullstack JS/TS Engineer',
   fullname: 'Adam Emmanuel',
   status: 'available',
-  occupation: 'Looking for new opportunities',
   skills: ['html/css', 'js', 'ts', 'react', 'node', 'next.js', 'nest.js', 'tailwind'],
   avatar: '/images/avatar.webp',
 };
 
 export default function BusinessCard() {
-  // TODO remove text => fetch card data
+  const { title, fullname, status, skills, avatar } = presentation;
   return (
     <div className="flex flex-col w-full md:max-w-4/5 p-5 gap-6 shadow-basic shadow-base-shadow rounded-lg mb-4">
       <header className="flex justify-between">
-        <Avatar src={presentation.avatar} />
-        <Tag>{presentation.status}</Tag>
+        <Avatar src={avatar} />
+        <Tag>{status}</Tag>
       </header>
       <div className="flex flex-col gap-1">
-        <span className="text-lg font-semibold">{presentation.fullname}</span>
-        <span className="text-sm">{presentation.title}</span>
+        <span className="text-lg font-semibold">{fullname}</span>
+        <span className="text-sm">{title}</span>
       </div>
       <footer>
         <ul className="flex gap-2 flex-wrap">
-          {presentation.skills.map((skill) => <li key={skill} className="uppercase text-sm tracking-normal border border-base-border bg-panel py-1 px-2 rounded-full">{skill}</li>)}
+          {skills.map((skill) => <li key={skill} className="uppercase text-sm tracking-normal border border-base-border bg-panel py-1 px-2 rounded-full">{skill}</li>)}
         </ul>
       </footer>
     </div>
