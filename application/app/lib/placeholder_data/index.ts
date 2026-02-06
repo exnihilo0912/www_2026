86930d662f0a7dee0870bbff7524b9c48262f32f
Adam EMMANUEL <adam.emmanuel.pro@gmail.com>
2026-02-06 11:58:05 +0100

    [feat] Update work experience schema and data

--- application/app/lib/placeholder_data/index.ts
+++ application/app/lib/placeholder_data/index.ts
@@ -7,17 +7,42 @@ export interface WorkExperience {
   work_from: WorkLocation;
   individual_contributor_seniority: IndividualContributorSeniority;
   management_seniority?: ManagementSeniority;
+  specialties?: Specialty[];
   start_date: string;
   end_date?: string;
   skills: string[]; // TODO replace with "skill" type (name, type [library, language, software, framework, concept, ...])
   description: string; // TODO replace with "tasks/missions" type
 }
+export type Specialty = 'frontend' | 'backend' | 'full_stack' | 'mobile' | 'devops' | 'data_engineer';
+export const specialties: { id: Specialty; label: string; }[] = [
+  { id: 'frontend', label: 'Frontend Engineer' },
+  { id: 'backend', label: 'Backend Engineer' },
+  { id: 'full_stack', label: 'Full Stack Engineer' },
+  { id: 'mobile', label: 'Mobile Developer' },
+  { id: 'devops', label: 'DevOps Engineer' },
+  { id: 'data_engineer', label: 'Data Engineer' },
+];
 export interface Entity {
   id: string;
   name: string;
   url: string;
   logo?: string;
+  industry?: string;
+  size?: CompanySize;
+  project_types?: string[];
 }
+export type CompanySize = '1' | '2-10' | '10-50' | '50-100' | '100-500' | '500-1000' | '1000-5000' | '5000-10000' | '10000+';
+export const companySizes: { id: CompanySize; label: string; }[] = [
+  { id: '1', label: 'Solo Founder' },
+  { id: '2-10', label: 'Micro Startup' },
+  { id: '10-50', label: 'Early Stage Startup' },
+  { id: '50-100', label: 'Scale-Up' },
+  { id: '100-500', label: 'Mid Sized Company' },
+  { id: '500-1000', label: 'Large Company' },
+  { id: '1000-5000', label: 'Enterprise' },
+  { id: '5000-10000', label: 'Large Enterprise' },
+  { id: '10000+', label: 'Multinational Corporation' },
+];
 export interface Location {
   country: string;
   city?: string;
@@ -29,24 +54,28 @@ export type ManagementSeniority = 'manager' | 'senior_manager' | 'director' | 's
 
 export const entities: Entity[] = [
   {
-    id: 'lemlist',
-    name: 'Lemlist',
-    url: 'https://www.lemlist.com',
-  },
-  {
     id: 'tridge',
     name: 'Tridge',
     url: 'https://www.tridge.com',
+    industry: 'AgroTech',
+    size: '100-500',
+    project_types: ['B2B SaaS', 'Back Office'],
   },
   {
     id: 'custeed',
     name: 'Custeed',
     url: 'https://www.custeed.com',
+    industry: 'AutoTech',
+    size: '10-50',
+    project_types: ['B2B SaaS', 'Back Office'],
   },
   {
     id: 'nexity',
     name: 'Nexity',
     url: 'https://www.nexity.fr',
+    industry: 'Real Estate',
+    size: '1000-5000',
+    project_types: ['Front Office'],
   },
 ];
 export const workExperiences: WorkExperience[] = [
@@ -62,6 +91,7 @@ export const workExperiences: WorkExperience[] = [
     skills: ['JavaScript', 'TypeScript', 'React', 'Meteor'],
     description: 'blabla',
     individual_contributor_seniority: 'mid_level',
+    specialties: ['data_engineer', 'full_stack'],
   },
   {
     id: 'three',
@@ -75,6 +105,7 @@ export const workExperiences: WorkExperience[] = [
     skills: ['JavaScript', 'TypeScript', 'React', 'Meteor'],
     description: 'blabla',
     individual_contributor_seniority: 'mid_level',
+    specialties: ['frontend'],
   },
   {
     id: 'four',
@@ -88,6 +119,7 @@ export const workExperiences: WorkExperience[] = [
     skills: ['JavaScript', 'TypeScript', 'React', 'Meteor'],
     description: 'blabla',
     individual_contributor_seniority: 'mid_level',
+    specialties: ['frontend'],
   },
 ];
 
