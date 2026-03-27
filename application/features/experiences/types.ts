f6da8ba6b008d1574c59ebca14297a1525c34cd9
Adam EMMANUEL <adam.emmanuel.pro@gmail.com>
2026-03-27 18:14:22 +0100

    [feat] Add product and product_spec typing

--- application/features/experiences/types.ts
+++ application/features/experiences/types.ts
@@ -14,7 +14,9 @@ export interface WorkExperience {
   specialties?: Specialty[];
   start_date: string;
   end_date?: string;
-  work_experience_detail?: WorkExperienceDetail;
+  description: string; // global description
+  product?: Product;
+  work_experience_details?: WorkExperienceDetail[]; // detail breakdown (different role/ horizontal tasks)
 }
 export type Specialty = 'frontend' | 'backend' | 'full_stack' | 'mobile' | 'devops' | 'data_engineer';
 export type CompanySize = '1' | '2-10' | '10-50' | '50-100' | '100-500' | '500-1000' | '1000-5000' | '5000-10000' | '10000+';
@@ -23,13 +25,43 @@ export interface Location {
   city?: string;
 }
 export interface WorkExperienceDetail {
+  id: string;
   title: string;
   description: string;
   technologies?: Technology[];
   skills?: Skill[];
   challenges?: string[];
   achievements?: string[];
+  product?: Product;
 }
+
+export interface Product {
+  id: string;
+  name: string;
+  types: ProductType[];
+  product_specifications: ProductSpec;
+}
+export type ProductType = 'SaaS' | 'SaaS B2B' | 'SaaS B2C'; // TODO complete the list
+
+export interface ProductSpec {
+  id: string;
+  infrastructure: InfrastructureType;
+  app_model: ApplicationModelType;
+  rendering_strategy: RenderingStrategyType;
+  frontend_architecture: FrontendArchitectureType;
+  backend_architecture: BackendArchitectureType;
+  state_management: StateManagementType;
+  ui_pattern: UIPatternType;
+  api_type: APIType;
+}
+type InfrastructureType = 'monolith' | 'micro-services'; // TODO finish list
+type ApplicationModelType = 'SPA' | 'MPA';
+type RenderingStrategyType = 'CSR' | 'SSR' | 'SSG' | 'ISR' | 'SSR - hybrid';
+type UIPatternType = 'MVC' | 'Flux-Redux' | 'MVVM' | 'VIPER'; // TODO finish list
+type FrontendArchitectureType = 'feature-based' | 'type-base' | 'feature-sliced' | 'page-based'; // TODO finish list
+type BackendArchitectureType = 'hexagonal' | 'layered'; // TODO finish list
+type StateManagementType = ''; // TODO
+type APIType = 'REST' | 'GraphQL' | 'SOAP' | 'gRPC'; // TODO
 
 export interface Skill {
   name: string;
